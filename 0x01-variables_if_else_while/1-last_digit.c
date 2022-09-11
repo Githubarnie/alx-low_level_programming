f9c7656c515b8986c7430748965a382ea05170a9
Githubarnie <amapianorupiya@gmial.com>
2022-09-11 09:23:19 -0700

    Task 1. TheLast digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ printf("Last digit of %i is %i and is 0\n", n, l);
 }
 else
 {
-printf("Last digit of %i is %i and is less than 6 and not 0\n");
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
