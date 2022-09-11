abe90924bcc7942fb324cff94c8bbc67acd85dc8
Githubarnie <amapianorupiya@gmial.com>
2022-09-11 09:42:42 -0700

    Task 2. alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ * main - print the alphabet starting at a
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, negative
+ * Return: 0
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a' ; alpha <= 'z')
+{
+putchar(alpha);
+}
+putchar("\n");
+return (0);
+}
