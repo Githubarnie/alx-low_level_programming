da042bfdf393fcc82c839122ba3a579832f7d597
Githubarnie <amapianorupiya@gmial.com>
2022-09-12 14:23:11 -0700

    Task .8  numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
  * main - prints  if the number is positive, zero, negative
  *
  * Description: using the main function
- * this program prints "The numbers until 0-9 combine with letter to make it base 16
+ * this program prints "The numbers until 0-9 combine with letter 
  * Return: 0
  *
  */
