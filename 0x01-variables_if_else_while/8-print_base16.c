b63e4b7703e641433467a8c5a015ef4b2cdf7fe5
Githubarnie <amapianorupiya@gmial.com>
2022-09-13 00:09:29 -0700

    Task 8. Single numbers with letters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
  * main - prints if the number is positive, zero, negative
  *
  * Description: using the main function
- * this program prints "The numbers until 0-9 combine with letter 
+ * this program prints "The number combine with letter
  * Return: 0
  */
 int main(void)
