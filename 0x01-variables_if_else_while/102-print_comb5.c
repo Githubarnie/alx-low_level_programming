0c6087c5edce7aa74e5458a1b0389fb78a5f12b3
Githubarnie <amapianorupiya@gmial.com>
2022-09-13 13:42:39 -0700

    Task 12. possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Descrption: using the main funtion
+ * this program prints"possible combinations of two two-digit
+ */
+int main(void)
+{
+int num1 = 0,num2;
+while (num1 <= 99)
+{
+num2 = num1;
+while (num2 <= 99)
+{
+if (num2 != num1)
+{
+/** the %-sign to check if we have any reminder */
+putchar((num1 / 10) + 48);
+putchar((num1 % 10) + 48);
+putchar(' ');
+putchar((num2 / 10) + 48);
+putchar((num2 % 10) + 48);
+if(num1 !=98 || num2 != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+++num2;
+}
+++num1;
+}
+putchar('\n');
+return (0);
+}
