0cfcf1ef3e4fcd476269803b0add769d68bacc80
Githubarnie <amapianorupiya@gmial.com>
2022-09-13 12:15:28 -0700

    Task 10.  all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 int num1 = 0, num2;
-while(num1 <= 9)
+while (num1 <= 9)
 {
 num2 = 0;
-while(num2 <= 9)
+while (num2 <= 9)
 {
-if(num1 != num2 && num1 < num2)
+if (num1 != num2 && num1 < num2)
 {
 putchar(num1 + 48);
 putchar(num2 + 48);
-if(num1 + num2 != 17)
+if (num1 + num2 != 17)
 {
 putchar(',');
 putchar(' ');
