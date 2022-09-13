d55f19ec78463cca98581959cb88400010670362
Githubarnie <amapianorupiya@gmial.com>
2022-09-13 13:12:11 -0700

    Task 11. all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,7 @@
  * main - print the positive three digits
  *
  * Description: using the main function
- * this program will print " all possible different combinations of three digits.
+ * this program will print " all possible different combinations of three digit
  * Return: 0
  */
 int main(void)
@@ -18,12 +18,12 @@ while (num2 <= 9)
 num3 = 0;
 while (num3 <= 9)
 {
-if(num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
+if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
 {
 putchar(num1 + 48);
 putchar(num2 + 48);
 putchar(num3 + 48);
-if(num1 + num2 + num3 != 24)
+if (num1 + num2 + num3 != 24)
 {
 putchar(',');
 putchar(' ');
