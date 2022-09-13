f947a73b911957c0b850cc0d247453ec1c08e720
Githubarnie <amapianorupiya@gmial.com>
2022-09-13 00:33:49 -0700

    Task 9. Numbers sepated by comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 int num;
-for (num = '0'; num = '9'; num++)
+for (num == '0'; num <= '9'; num++)
 {
 putchar(num);
-putchar(', ');
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
